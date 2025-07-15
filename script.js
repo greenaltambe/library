d2f4ca8e67caa4cc7c8583ca76e8a91462bf5a5a
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-07-15 22:07:31 +0530

    Set up

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+}
+
+function addBookToLibrary() {
+	// take params, create a book then store it in the array
+	const book = new Book(title, author, pages, read);
+	myLibrary.push(book);
+}
