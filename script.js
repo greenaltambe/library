9f49e81639a0ab2f89005384dcd5fb677dd2ba05
greenaltambe <154662822+greenaltambe@users.noreply.github.com>
2025-07-16 11:35:34 +0530

    Add dialogue box

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const myLibrary = [];
 
+// html
+const newBookButton = document.getElementById("new-book");
+const dialog = document.getElementById("dialog");
+
+newBookButton.addEventListener("click", () => {
+	dialog.showModal();
+});
+
 function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
